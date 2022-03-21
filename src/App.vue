0bd6e48832b0b13aeef28af230aed9db9fa1e7e3
i.uskov <i.uskov@whitewill.ru>
2022-03-21 22:35:42 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>  
+  <Simon/>
+</template>
+
+<script>
+import 'animate.css';
+import Simon from './components/Simon.vue';
+
+export default {
+  /* Component's name */
+  name: 'App',
+
+  /* Components */
+  components: {
+    Simon
+  }
+}
+</script>
+
+<style lang="scss">
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    padding: 1rem;
+    overflow: hidden;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
